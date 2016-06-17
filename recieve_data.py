86b70c108c3511075d495d5b327bb92a64342655
Arunabh98 <arunabhghosh98@gmail.com>
2016-06-18 01:05:23 +0530

    Add bot image

--- recieve_data.py
+++ recieve_data.py
@@ -1,5 +1,5 @@
-
 import bluetooth
+import re
 server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 port = 1
 server_sock.bind(("", port))
@@ -7,8 +7,22 @@ server_sock.listen(1)
 
 client_sock, address = server_sock.accept()
 print "Accepted connection from ", address
+map_width = 13
+map_environment = [[0.00 for i in range(map_width)] for j in range(map_width)]
 
 while True:
-
     data = client_sock.recv(1024)
-    print "received [%s]" % data
+    row = 0
+    column = 0
+    for prob in re.findall(r"[-+]?\d*\.\d+|\d+", data):
+        map_environment[row][column] = float(prob)
+        column += 1
+        if column == map_width:
+            column = 0
+            row += 1
+        if row == map_width:
+            break
+    for x in map_environment:
+       print x
+    print data[-1]
+    print ""
